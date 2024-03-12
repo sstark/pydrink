73f4222aed5c2b608cb7d423f6d47e10e533f53f
Sebastian Stark <sstark@mailbox.org>
2024-03-13 00:22:24 +0100

    First half of show_untracked_files()

--- src/pydrink/drink.py
+++ src/pydrink/drink.py
@@ -1,6 +1,49 @@
 #!/usr/bin/env python
 
 from subprocess import run, CalledProcessError
+from pathlib import Path
+import os
+
+KINDS = {
+    "bin":      "bin",
+    "zfunc":    ".zfunc",
+    "conf":     "."
+}
+
+def warn(s):
+    print(s)
+
+def show_untracked_files(selected_kind=None):
+    '''Show untracked files / possible drink candidates'''
+    # 1. Differenziere, welches $kind global über die cli angegeben wurde
+    #   a. alle kinds
+    #   b. nur ein kind
+    # 2. Iteriere über alle angegebenen kinds
+    #   1. Finde targetdir für kind (get_kinddir())
+    #       -> Exception wenn nicht vorhanden
+    #   2. Fallunterscheidung:
+    #       a. kind=conf: kindfiles = $targetdir/.*
+    #       b. ansonsten: kindfiles = $targetdir/*
+    #   3. Iteriere über alle kindfiles:
+    #       0. Schneide Pfad ab, also /home/bla/bin/foo -> foo
+    #       1. Wenn in "ignore", skippen
+    #       2. Wenn der filename einem key aus KINDDIR entspricht, skippen
+    #       3. is_tracked(file, kind)
+    #       Jetzt je nach tracking status ausgeben
+
+    os.chdir("/home/seb")
+    for kind, targetdir in KINDS.items():
+        # Skip the others, if user has selected only a certain kind
+        if selected_kind and selected_kind != kind:
+            warn(f"skipping {kind}")
+            continue
+        if kind == "conf":
+            pat = ".*"
+        else:
+            pat = "*"
+        for f in Path(targetdir).glob(pat):
+            print(f)
+
 
 def cli():
     try:
@@ -9,3 +52,4 @@ def cli():
     except CalledProcessError as e:
         print(f"Error {e.returncode}\n{result.stderr}")
     print(result.stdout)
+    show_untracked_files()
