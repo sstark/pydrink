774fbad06e0ee9463e368b3a5c51a1b4b88a5a6c
Sebastian Stark <sstark@mailbox.org>
2024-03-18 06:19:49 +0100

    Use parametrize

--- tests/pydrink/git.py
+++ tests/pydrink/git.py
@@ -1,6 +1,7 @@
 import sys
 from pydrink.config import Config, BY_TARGET
 from pydrink.git import get_branches, menu, unclean
+import pytest
 
 
 def test_unclean_repo(tracked_drinkrc_and_drinkdir):
@@ -17,26 +18,11 @@ def test_get_branches(tracked_drinkrc_and_drinkdir):
     assert branches == ["hostA/master", "hostB/master", "hostC/master"]
 
 
-def test_git_menu_quit(tracked_drinkrc_and_drinkdir):
-    c = Config(tracked_drinkrc_and_drinkdir)
-    assert menu(c, lambda _: "1") == 0
-
-
-def test_git_menu_fetch(tracked_drinkrc_and_drinkdir):
+@pytest.mark.parametrize("test_input,expected", [("1", 0), ("2", 0), ("3", 0),
+                                                 ("99", 99)])
+def test_git_menu_items(tracked_drinkrc_and_drinkdir, test_input, expected):
     # We will get a busy loop if we replace input() and are on a tty.
     # So let's pretend we are not on a tty.
     sys.stdin.isatty = lambda: False
     c = Config(tracked_drinkrc_and_drinkdir)
-    assert menu(c, lambda _: "2") == 0
-
-
-def test_git_menu_push(tracked_drinkrc_and_drinkdir):
-    sys.stdin.isatty = lambda: False
-    c = Config(tracked_drinkrc_and_drinkdir)
-    assert menu(c, lambda _: "3") == 0
-
-
-def test_git_menu_invalid_item(tracked_drinkrc_and_drinkdir):
-    sys.stdin.isatty = lambda: False
-    c = Config(tracked_drinkrc_and_drinkdir)
-    assert menu(c, lambda _: "99") == 99
+    assert menu(c, lambda _: test_input) == expected
