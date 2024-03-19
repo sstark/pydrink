3efe7a6a52635f8e90e45058d995e6ecaf25a33f
Sebastian Stark <sstark@mailbox.org>
2024-03-19 21:03:37 +0100

    Update class doc to be more clear

--- src/pydrink/obj.py
+++ src/pydrink/obj.py
@@ -23,14 +23,22 @@ class DrinkObject():
 
     A drink object can be a "binary", a zsh function or configuration file.
 
-    Actual drink objects are those that exist in the drink repository. They can
-    be already symlinked into place or have a symlink operation pending.
+    Description of the different object states
+    ------------------------------------------
 
-    Potential drink objects are those that do not exist in the drink repository, but
-    are valid candidates.
+    Actual drink objects are those that exist in the drink repository. They can
+    be already symlinked into place. (ManagedHere)
+    
+    Or objects exist in the drink repository and have a symlink operation
+    pending, which means they would be linked with the next "drink -l" run.
+    (ManagedHerePending)
 
     A mixed form is an object that is not present on the current system (not
-    symlinked), but is a drink object for another target (another host).
+    symlinked), but is a drink object for another target or globally.
+    (ManagedOther)
+
+    Potential drink objects are those that do not exist in the drink repository, but
+    are valid candidates. (Unmanaged)
 
     Target in this context always means a context in which an object should be
     symlinked into the environment, usually but not necessarily a hostname.
