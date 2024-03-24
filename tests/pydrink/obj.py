8df57db03c7cbfcfb9b3f1f56668b58482c4dc1f
Sebastian Stark <sstark@mailbox.org>
2024-03-24 22:09:26 +0100

    Test detect_relpath

--- tests/pydrink/obj.py
+++ tests/pydrink/obj.py
@@ -5,13 +5,34 @@ import pytest
 
 
 @pytest.mark.parametrize(
-    "obj_p, str_p",
-    [(Path("bin") / BY_TARGET / "foo" / "obj1",
-      str(Path.home() / "bin" / "obj1")),
-     (Path("bin") / "obj2",
-      str(Path.home() / "bin" / "obj2")),
-     (Path("conf") / BY_TARGET / "bapf" / "obj4",
-      str(Path.home() / "." / "obj4"))])
+    "obj_p, relpath",
+    [(Path("bin") / BY_TARGET / "foo" / "obj1", Path("obj1")),
+     (Path("bin") / "obj2", Path("obj2")),
+     (Path("conf") / BY_TARGET / "bapf" / "obj4", Path("obj4"))])
+def test_detect_relpath(tracked_drinkrc_and_drinkdir, obj_p, relpath):
+    c = Config(tracked_drinkrc_and_drinkdir)
+    obj = DrinkObject(c, c["DRINKDIR"] / obj_p)
+    assert obj.relpath == relpath
+
+
+def test_detect_kind():
+    pass
+
+
+def test_detect_target():
+    pass
+
+
+def test_detect_state():
+    pass
+
+
+@pytest.mark.parametrize(
+    "obj_p, str_p", [(Path("bin") / BY_TARGET / "foo" / "obj1",
+                      str(Path.home() / "bin" / "obj1")),
+                     (Path("bin") / "obj2", str(Path.home() / "bin" / "obj2")),
+                     (Path("conf") / BY_TARGET / "bapf" / "obj4",
+                      str(Path.home() / "." / "obj4"))])
 def test_get_linkpath(tracked_drinkrc_and_drinkdir, obj_p, str_p):
     c = Config(tracked_drinkrc_and_drinkdir)
     obj = DrinkObject(c, c["DRINKDIR"] / obj_p)
